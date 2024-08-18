ee3b3d6e2727dd2774ce29110e67d6d69172c8fb
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-08-18 16:16:58 -0400

    complete

--- src/main/java/jom/com/softserve/s6/task3/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task3/MyUtils.java
@@ -1,11 +1,21 @@
 package jom.com.softserve.s6.task3;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class MyUtils {
 
-    //Write your code here
+    public static void writeFile(String filename, String text) {
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            for (char c : text.toCharArray()) {
+                String binaryString = String.format("%7s", Integer.toBinaryString(c)).replace(' ', '0');
+                fos.write(binaryString.getBytes());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
