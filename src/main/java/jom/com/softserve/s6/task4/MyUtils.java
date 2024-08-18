fdbd777efe4e35a10bbb7846021f94e0735659fe
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-08-18 16:27:18 -0400

    complete

--- src/main/java/jom/com/softserve/s6/task4/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task4/MyUtils.java
@@ -8,6 +8,26 @@ import java.util.Locale;
 
 public class MyUtils {
 
-    //Write your code here
+    public static String readFile(String filename) {
+
+        try (var stream = new FileInputStream(filename)) {
+            var builder = new StringBuilder();
+
+            byte[] bytes;
+            while (stream.available() > 0) {
+                bytes = stream.readNBytes(7);
+                String str = new String(bytes);
+                int integer = Integer.parseInt(str, 2);
+                char character = (char) integer;
+
+                builder.append(character);
+            }
+
+            return builder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
