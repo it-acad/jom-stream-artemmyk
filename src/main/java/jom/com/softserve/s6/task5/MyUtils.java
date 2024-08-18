e3a17465e1f1804b23c745fb524f9f5fff0a6f2e
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-08-18 19:40:18 -0400

    complete

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -7,6 +7,18 @@ import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
+    public Stream<String> nameList(Map<String, Stream<String>> map) {
+        return map
+                .values()
+                .stream()
+                .filter(Objects::nonNull)
+                .reduce(Stream.empty(), Stream::concat)
+                .filter(Objects::nonNull)
+                .map(str -> str.replace(" ", ""))
+                .filter(str -> !str.isEmpty())
+                .map(str -> str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase())
+                .distinct()
+                .sorted();
+    }
 
 }
