8011b31db54aab860b5594e8b9564483f857b7fb
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-08-18 20:25:16 -0400

    complete

--- src/main/java/jom/com/softserve/s6/task7/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task7/MyUtils.java
@@ -1,6 +1,7 @@
 package jom.com.softserve.s6.task7;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,6 +9,22 @@ import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
+    public Stream<Integer> duplicateElements(Stream<Integer> stream) {
+        var visited = new HashSet<Integer>();
+        var saved = new HashSet<Integer>();
+        return stream
+                .filter(Objects::nonNull)
+                .sorted()
+                .filter(val -> {
+                    if (visited.contains(val) && !saved.contains(val)) {
+                        saved.add(val);
+                        return true;
+                    }
+
+                    visited.add(val);
+
+                    return false;
+                });
+    }
 
 }
